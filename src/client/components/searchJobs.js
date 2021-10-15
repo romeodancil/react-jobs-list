9b2d3fc791ada7af5c93e32f03e241843c619530
Romeo Dancil <romeodancil30@gmai.com>
2021-10-15 23:45:21 +0800

    add filter from last 7 days ago

--- src/client/components/searchJobs.js
+++ src/client/components/searchJobs.js
@@ -7,21 +7,62 @@ class SearchJobs extends Component {
     constructor(props) {
         super(props)
         this.handleSearch = this.handleSearch.bind(this)
+        this.handleToggle = this.handleToggle.bind(this)
+        this.state = {
+            toggle: false
+        }
     }
 
     handleSearch(e){
-        let filter = this.props.jobs.filter((job) => job.companyName.toLowerCase().includes(e.target.value.toLowerCase()));
+        const filter = this.props.jobs.filter((job) => job.companyName.toLowerCase().includes(e.target.value.toLowerCase()));
         this.props.filterJobs(filter)
-        console.log('moment', moment().subtract(7, 'days'));
+    }
+
+    handleToggle(e) {
+        const { jobs } = this.props
+        const checked = e.target.checked
+        this.setState({
+            toggle: checked
+        })
+
+        if (checked) {
+            const final = moment().subtract(7,'d').format('YYYY-MM-DD');
+            const filterByLast7Days = this.props.jobs.filter((filt) => final <= moment(filt.OBJpostingDate).format('YYYY-MM-DD'))
+            this.props.filterJobs(filterByLast7Days)
+        } else {
+            this.props.filterJobs(jobs)
+        }
     }
 
     render() {
         return (
             <div class="row">
-                <div className="input-field col s12">
-                    <i className="material-icons prefix">search</i>
-                    <input onChange={this.handleSearch} placeholder="Enter Company Name" id="company_name" type="text" className="validate" />
-                    <label className="active" htmlFor="company_name">Search By Company Name</label>
+                <div className="col s12">
+                    <div class="row">
+                        <div className="input-field col s12">
+                            <i className="material-icons prefix">search</i>
+                            <input onChange={this.handleSearch} placeholder="Enter Company Name" id="company_name" type="text" className="validate" />
+                            <label className="active" htmlFor="company_name">Search By Company Name</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="col s12">
+                    <div class="row">
+                        <div className="col s12">
+                            <label>
+                                <i className="material-icons left">today</i>
+                                Filter Post From Last 7 Days
+                            </label>
+                            <div class="switch">
+                                <label>
+                                Off
+                                <input checked={this.state.toggle} onChange={this.handleToggle} type="checkbox"/>
+                                <span className="lever"></span>
+                                On
+                                </label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
